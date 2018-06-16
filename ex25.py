d6a8ec1c1efa8fbb7c069e217b5168386732476a
DarkTamara <38437336+DarkTamara@users.noreply.github.com>
2018-06-16 17:23:56 +0300

    Add files via upload

--- ex25.py
+++ ex25.py
@@ -1,29 +1,30 @@
 def break_words (stuff):
     """this function wll break up the words for us"""
-    words = stuff.split(' ')
+    words = stuff.split(' ') #splits the "stuff" argument i will give to him
     return words
 
 def sort_words(words):
-    return sorted(words)
+    return sorted(words) #sorts the things in the list
 
 def print_first_word(words):
     """prints the first word after popping it off."""
-    word = words.pop(0)
+    word = words.pop(0) # this one takes the element on the 0 possition and deletes it
     print word
 
 def print_last_word(words):
     """prints the last word after popping it off."""
+    word = words.pop(len(words)-1)
     print word
 
 def sort_sentence(sentence):
     """takes in a full sentance and returns the sorted words."""
-    return sort_words(words)
+    return sort_words(words) #sorts stuff 
 
 def print_first_and_last (sentence):
     """prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
-    print_last_word(word)
+    print_last_word(words)
 
 def print_first_and_last_sorted(sentence):
     """sorts the words then prints the first and last one."""
